330b85cd4493c8f46ddbfa80e3788967ba4ade40
platonvadim <vadim.platon97@gmail.com>
2024-04-09 17:52:18 +0300

    upload homework4

--- 03-lesson/task_3.py
+++ 03-lesson/task_3.py
@@ -2,80 +2,87 @@
 # Creează o variabilă numită `number` și atribuie-i valoarea `10`
 
 # CODUL TĂU VINE MAI JOS:
-
+number = 10
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișează valoarea variabilei `number` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(number)
 # CODUL TĂU VINE MAI SUS:
 
 # Acum creează o nouă variabilă numită `number2` și atribuie-i valoarea `number` și afișează valoarea variabilei `number2` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+number2 = number
+print(number2)
 # CODUL TĂU VINE MAI SUS:
 
 # Acum creează o nouă variabilă numită `number3` și atribuie-i valoarea `4`
 
 # CODUL TĂU VINE MAI JOS:
-
+number3 = 4
 # CODUL TĂU VINE MAI SUS:
 
 # Acum aplică toate operațiile pe care le-ai învățat în lecție asupra variabilelor `number` și `number3` și afișează rezultatele folosind funcția `print`
 # Operațiile sunt: adunare, scădere, înmulțire, împărțire, modulo, exponențiere, împărțire întreagă
 
 # CODUL TĂU VINE MAI JOS:
-
+print(number+1, number3+1)
+print(number-2, number3-1)
+print(number*3, number3*3)
+print(number/4, number3/4)
+print(number%5, number3%5)
+print(number**6, number3**6)
+print(number//7, number3//7)
 # CODUL TĂU VINE MAI SUS:
 
 # Creează o nouă variabilă numită `number4` și atribuie-i o valoare numerică mare într-un mod literal
 
 # CODUL TĂU VINE MAI JOS:
-
+number4 = 999_999_999_999
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișează tipul variabilei `number4` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(type(number4))
 # CODUL TĂU VINE MAI SUS:
 
 # Creați o variabilă `numar` și atribuiții valoarea 5.0
 
 # CODUL TĂU VINE MAI JOS:
-
+numar = 5.0
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișați tipul lui `numar` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(type(numar))
 # CODUL TĂU VINE MAI SUS:
 
 # Acum creați o variabilă `numar2` și atribuiții valoarea 2.0
 
 # CODUL TĂU VINE MAI JOS:
-
+numar2 = 2.0
 # CODUL TĂU VINE MAI SUS:
 
 # Acum creați o variabila `numar_mare` și atribuiți-i o valoare mare utilizând numerele literale
 
 # CODUL TĂU VINE MAI JOS:
-
+numar_mare = 987_654_321_000
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișați tipul lui `numar_mare` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(type(numar_mare))
 # CODUL TĂU VINE MAI SUS:
 
 # Acum ștergeți variabila `numar_mare` folosind instrucțiunea `del`
 
 # CODUL TĂU VINE MAI JOS:
-
+del numar_mare
 # CODUL TĂU VINE MAI SUS:
 
 # Asta a fost tot pentru această sarcină
---
+++ 04-lesson/task_2.py
@@ -0,0 +1,131 @@
+# Aceasta este a doua ta sarcină legată a leciei legate de stringuri în python
+
+# Creează o variabilă numită `name` și seteaz-o la numele tău
+
+# CODUL TĂU VINE MAI JOS:
+name = "Vadim"
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `name` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(name)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `name2` și seteaz-o la valoarea variabilei `name` și afișează valoarea variabilei `name2` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+name2 = name
+print(name2)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează ultimul caracter al variabilei `name` folosind indexarea
+
+# CODUL TĂU VINE MAI JOS:
+print(name[-1])
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează primele 3 caractere ale variabilei `name` folosind slicing
+
+# CODUL TĂU VINE MAI JOS:
+print(name[:3])
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează valoarea variabilei `name` în majuscule folosind metoda `upper`
+
+# CODUL TĂU VINE MAI JOS:
+print(name.upper())
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează valoarea variabilei `name` în minuscule folosind metoda `lower`
+
+# CODUL TĂU VINE MAI JOS:
+print(name.lower())
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează concatenarea variabilelor `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+print(name+name2)
+# CODUL TĂU VINE MAI SUS:
+
+# Creează o variabilă `text` și setează-i un text la alegere, cu restricția ca acesta să conțină mai multe rânduri
+
+# CODUL TĂU VINE MAI JOS:
+text = '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'''
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă variabila `text` conține cuvântul `python`
+
+# CODUL TĂU VINE MAI JOS:
+print("python" in text)
+# CODUL TĂU VINE MAI SUS:
+
+# Folosește metoda replace pentru a înlocui litera `a` din variabila `text` cu litera `e`
+
+# CODUL TĂU VINE MAI JOS:
+print(text.replace('a', 'e'))
+# CODUL TĂU VINE MAI SUS:
+
+# Folosește metoda split pentru a transforma variabila `text` într-o listă de cuvinte
+
+# CODUL TĂU VINE MAI JOS:
+print(text.split())
+# CODUL TĂU VINE MAI SUS:
+
+# Creează un f-string care să conțină variabilele `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+fstring = f"Hello {name} and {name2}!"
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă string-ul creat se termină cu `!`
+
+# CODUL TĂU VINE MAI JOS:
+print(fstring.endswith("!"))
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă string-ul creat începe cu `Hello`
+
+# CODUL TĂU VINE MAI JOS:
+print(fstring.startswith("Hello"))
+
+# CODUL TĂU VINE MAI SUS:
+
+# Identifică indexul unde se află `!` în string-ul creat
+
+# CODUL TĂU VINE MAI JOS:
+print(fstring.index("!"))
+# CODUL TĂU VINE MAI SUS:
+
+# Identifică indexul unde se află `o` în string-ul creat folosind altă metodă
+
+# CODUL TĂU VINE MAI JOS:
+print(fstring.find("!"))
+# CODUL TĂU VINE MAI SUS:
+
+# Utilizând format string-uri, creează un string care să conțină variabilele `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+format_string = "Hello {} and {}!".format(name, name2)
+# CODUL TĂU VINE MAI SUS:
+
+# Concatenează string-ul creat cu string-ul `text`
+
+# CODUL TĂU VINE MAI JOS:
+text += format_string
+# CODUL TĂU VINE MAI SUS:
+
+# Afișează lungimea string-ului creat
+
+# CODUL TĂU VINE MAI JOS:
+print(len(text))
+# CODUL TĂU VINE MAI SUS:
+
+# Aceasta a fost tot pentru această sarcină
+
